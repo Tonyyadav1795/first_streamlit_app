156e056f862465e377e09d26028c6b89457563b0
Tonyyadav1795 <131412471+Tonyyadav1795@users.noreply.github.com>
2023-12-25 12:33:39 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,44 @@
-import streamlit
-import pandas
+import streamlit as st
+import pandas as pd
 import requests
 import snowflake.connector
 
-streamlit.title('My Parents New Healthy Dinner')
+# Changed title as per the image instruction
+st.title('View Our Fruit List')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞Avocado Toast')
+st.header('Breakfast Menu')
+st.text('🥣Omega 3 & Blueberry Oatmeal')
+st.text('🥗Kale, Spinach & Rocket Smoothie')
+st.text('🐔Hard-Boiled Free-Range Egg')
+st.text('🥑🍞Avocado Toast')
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 # Assuming fruit_macros.txt is a CSV file with a 'Fruit' column
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page
-streamlit.dataframe(fruits_to_show)
+st.dataframe(fruits_to_show)
 
 # Function to get data from Fruityvice API
 def get_fruityvicedata(this_fruit_choice):
     fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{this_fruit_choice}")
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
 
-streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?')
+st.header('Fruityvice Fruit Advice!')
+fruit_choice = st.text_input('What fruit would you like information about?')
 if fruit_choice:
     back_from_function = get_fruityvicedata(fruit_choice)
-    streamlit.dataframe(back_from_function)
+    st.dataframe(back_from_function)
+
+# Connect to Snowflake
+my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 
 # Function to get fruit load list from Snowflake
 def get_fruit_load_list():
@@ -48,21 +52,19 @@ def insert_row_snowflake(new_fruit):
         my_cur.execute("insert into fruit_load_list values (%s)", (new_fruit,))
         return f"Thanks for adding {new_fruit}"
 
-# Connect to Snowflake
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-
 # Add a button to load the fruit list
-if streamlit.button('Get Fruit Load List'):
-    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+# Changed button label as per the image instruction
+if st.button('Add Your Favorites'):
+    my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
     my_cnx.close()
-    streamlit.dataframe(my_data_rows)
+    st.dataframe(my_data_rows)
 
 # Input for adding a new fruit
-add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-if streamlit.button('Add a Fruit to the List') and add_my_fruit:
+add_my_fruit = st.text_input('What fruit would you like to add?')
+if st.button('Add a Fruit to the List') and add_my_fruit:
     back_from_function = insert_row_snowflake(add_my_fruit)
-    streamlit.text(back_from_function)
+    st.text(back_from_function)
 
 # Adding new fruits as specified
 insert_row_snowflake("jackfruit")
